a73c7dd1a5d22ecdeabdb63c79d94dd7471a0b9b
Joshua Hamilton <joshua@tinydog.io>
2021-02-11 01:03:58 +0000

    Full install script & README update.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Exercism CLI Setup Script for Linux
 
-This *should* install Exercism CLI on any Linux system. However, this has been specifically created to allow us to set it up in Docker containers to be used in VSCode devcontainers. It may work for you, it may not.
+This *should* install Exercism CLI on any Linux system. However, this has been specifically created to allow us to set it up in Docker containers to be used in VSCode dev containers. It may work for you, it may not.
 
 ## Instructions
 
--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-echo 'Install'
+echo ' '
+echo '================================='
+echo '👻 Installing Exercism'
+echo '================================='
+echo ' '
+
+# Setup & Download ------------------------------------
+
+cd /tmp
+curl -LJ https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz -o exercism.tar.gz
+mkdir -p /tmp/exercism
+
+# Extract & Move --------------------------------------
+
+tar -xf exercism.tar.gz -C /tmp/exercism
+sudo mv /tmp/exercism/exercism /usr/local/bin/exercism
+
+# Cleanup ---------------------------------------------
+rm -rf /tmp/exercism
+rm exercism.tar.gz
+
+echo ' '
+echo 'Exercism Install Complete 👏'
+echo '---------------------------------'
+echo ' '
